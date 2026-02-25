3616ba08e448c163d020e4a955e6e7e43b444115
Macohi <sinconsistencia@gmail.com>
2026-02-25 16:49:27 -0600

    file suffix

--- zip.bat
+++ zip.bat
@@ -1,11 +1,12 @@
 @echo off
 set /P directory="Enter directory (example: debug/hl): "
+set /P suffix="Enter file suffix (example: z1-hashlink): "
 
 echo Moving to directory: "export/%directory%/bin/"
 cd "export/%directory%/bin/"
 
-echo Zipping directory:
-wsl zip -r Onrev *
+echo Zipping directory into "Onrev_%suffix%.zip":
+wsl zip -r "Onrev_%suffix%.zip" *
 
 echo Moving to export...
-wsl mv Onrev.zip ../../../../export/
+wsl mv "Onrev_%suffix%.zip" ../../../../export/
