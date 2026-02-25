148c1d9f46567dda10daf744481bf1c300f09595
Macohi <sinconsistencia@gmail.com>
2026-02-25 17:52:12 -0600

    blockID

--- source/Block.hx
+++ source/Block.hx
@@ -5,16 +5,16 @@ class Block extends FlxSprite
 	public static var SCALE:Float = 2;
 	public static var BLOCK_SIZE:Int = 16;
 
-	public var id:Int = 0;
+	public var blockID:Int = 0;
 
 	override public function new(id:Int, ?x:Float, ?y:Float)
 	{
 		super(x, y);
 
-		this.id = id;
+		this.blockID = id;
 
 		loadGraphic('assets/blocks.png', true, BLOCK_SIZE, BLOCK_SIZE);
-		animation.add('block', [this.id], 24);
+		animation.add('block', [this.blockID], 24);
 		animation.play('block');
 
 		this.scale.set(SCALE,SCALE);
--- source/InventoryScreen.hx
+++ source/InventoryScreen.hx
@@ -55,7 +55,7 @@ class InventoryScreen extends FlxSubState
 
 		for (block in inventoryBlocks.members)
 		{
-			if (CURRENT_ITEM == block.id)
+			if (CURRENT_ITEM == block.blockID)
 				block.colorTransform.redMultiplier = block.colorTransform.greenMultiplier = block.colorTransform.blueMultiplier = 1.1;
 			else
 				block.colorTransform.redMultiplier = block.colorTransform.greenMultiplier = block.colorTransform.blueMultiplier = 1.0;
