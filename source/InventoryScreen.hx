911a1be05886fb00e1ed31de445dd160a39cd0fb
Macohi <sinconsistencia@gmail.com>
2026-02-25 17:28:37 -0600

    get block count without hardcoding but with checking the block frames

--- source/InventoryScreen.hx
+++ source/InventoryScreen.hx
@@ -1,3 +1,5 @@
+import flixel.graphics.frames.FlxFrame;
+import flixel.graphics.FlxGraphic;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.FlxG;
@@ -10,6 +12,8 @@ class InventoryScreen extends FlxSubState
 
 	var whiteBG:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height);
 
+	var blockCount:Int = 0;
+
 	override function create()
 	{
 		super.create();
@@ -19,6 +23,28 @@ class InventoryScreen extends FlxSubState
 		whiteBG.screenCenter();
 
 		FlxTween.tween(whiteBG, {alpha: .5}, 0.15, {ease: FlxEase.quadInOut});
+
+		if (blockCount > 0)
+			return;
+
+		var graphics:Array<FlxFrame> = [];
+		var stopGettingGraphics:Bool = false;
+
+		var i = 0;
+		while (!stopGettingGraphics)
+		{
+			var b = new Block(i, 0, 0);
+
+			stopGettingGraphics = b.frame == null || graphics.contains(b.frame);
+
+			if (!graphics.contains(b.frame))
+				graphics.push(b.frame);
+
+			i++;
+		}
+
+		blockCount = graphics.length;
+		trace('Got $blockCount blocks!');
 	}
 
 	override function update(elapsed:Float)
