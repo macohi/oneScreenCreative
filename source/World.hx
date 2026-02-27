8a2cc6e93b7797373a91fbc42d3880c079da7776
Macohi <sinconsistencia@gmail.com>
2026-02-27 07:18:40 -0600

    parse starting x and y values

--- source/World.hx
+++ source/World.hx
@@ -1,4 +1,26 @@
+using StringTools;
+
 class World
 {
-	public static function parseWorldString(world:String) {}
+	// sx0_sy0_i1r10_i-1r10
+
+	public static function parseWorldString(world:String)
+	{
+		var game:PlayState = PlayState.instance;
+
+		trace('Parsing world: $world');
+
+		for (piece in world.split('_'))
+		{
+			trace(piece);
+
+			if (piece.startsWith('s'))
+			{
+				var symbol = piece.substr(1, 1);
+				var int = Std.parseInt(piece.substr(2));
+				
+				trace('STARTING $symbol : $int');
+			}
+		}
+	}
 }
