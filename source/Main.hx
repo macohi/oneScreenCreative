4faad0c19851896a0d5df153a8f8f84bc5a52cdb
Macohi <sinconsistencia@gmail.com>
2026-02-27 17:35:58 -0600

    why the fuck doesnt it work

--- source/Main.hx
+++ source/Main.hx
@@ -1,11 +1,7 @@
 package;
 
-import lime.utils.Assets;
-import flixel.FlxG;
 import flixel.FlxGame;
 import openfl.display.Sprite;
-
-using StringTools;
 
 class Main extends Sprite
 {
@@ -16,24 +12,5 @@ class Main extends Sprite
 		BlockManager.initBlocksStuff();
 
 		addChild(new FlxGame(0, 0, PlayState));
-
-		final tracks:Array<String> = Assets.getText('assets/tracks.txt').split('\n');
-
-		if (tracks.length < 1)
-			return;
-
-		for (i => track in tracks)
-			tracks[i] = track.trim();
-
-		FlxG.signals.postUpdate.add(function()
-		{
-			if (FlxG.sound.music?.playing)
-				return;
-			if (!FlxG.random.bool(FlxG.random.float(0, 15)))
-				return;
-
-			FlxG.sound.playMusic('assets/tracks/${tracks[FlxG.random.int(0, tracks.length - 1)]}.wav', 1.0, false);
-			FlxG.sound.music.fadeIn(1, 0, 1, function(t) {});
-		});
 	}
 }
